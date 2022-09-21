df32e2a29df15bd14ff6e85487441a9405b0a654
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2022-09-21 14:05:33 +0200

    feat: Añadido y configurado Redux

---
+++ src/Redux/types.js
@@ -0,0 +1,8 @@
+//Usuario
+export const LOGIN = 'login';
+export const LOGOUT = 'logout';
+export const MODIFY_CREDENTIALS = 'modify_credentials';
+
+//PROJECT
+
+export const PROJECT_DETAIL = 'project_detail';
